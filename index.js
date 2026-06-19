7de4990d5d687ce23452a5d0c9bbd15ec36c1b2f
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-06-19 15:21:10 +0600

    feat: implement MongoDB connection and user retrieval endpoint

--- index.js
+++ index.js
@@ -2,9 +2,46 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
 const port = process.env.PORT
+
+const uri = process.env.MONGO_DB;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    const myDB = client.db("BibloDrop");
+    const userCollection = myDB.collection("user")
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+    app.get("/user", async(req,res)=>{
+
+      const result = await userCollection.find().toArray()
+      res.send(result)
+      
+    })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
