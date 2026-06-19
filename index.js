fe56fec9a8229c0f31d009fb9bf3dc7f05324b41
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-06-19 15:02:50 +0600

    chore: add package.json with initial dependencies for bibliodrop-backend

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT
+
+
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
